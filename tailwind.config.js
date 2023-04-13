6f0816838a8f12278bab11716beab853766f317e
gaurav-shrestha <gvstha10@gmail.com>
2023-04-13 20:56:45 +0545

    screen size configured

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ["./*.html"],
+    screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "976px",
+        xl: "1440px",
+    },
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
